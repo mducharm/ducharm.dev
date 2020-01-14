fae6a73acc71070f880054adf8ff1a48c16fd84a
mducharm <9277274+mducharm@users.noreply.github.com>
2020-01-13 22:36:00 -0500

    updated about, added profile pic

---
+++ content/pages/about/ProfilePic.js
@@ -0,0 +1,11 @@
+import React from "react"
+import pic from './me+am.jpg'
+
+export default () => (
+  <div>
+    <img src={pic} alt="me+am" style={{
+      width: "200px",
+      height: "200px",
+    }} />
+  </div>
+)
